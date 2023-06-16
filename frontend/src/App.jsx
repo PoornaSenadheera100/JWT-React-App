a2b5dc754f617848bcb0b0459bedf6997ac6c637
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-06-16 10:27:31 +0530

    Implemented functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
+import MyList from "./components/MyList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const pvtKey =
+    "Bearer eyJhbGciOiJIUzI1NiJ9.e30.HPEtGdBfCLlAOYpKodUOLixujC6D_LKfrEy4mmfQ3Ok";
+
   function dataLoad() {
-    alert("Working");
+    axios
+      .get("http://localhost:8070/users/", {
+        headers: { Authorization: pvtKey },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
     <>
       <button onClick={dataLoad}>Load</button>
+      <br />
+      <br />
+      {users.length === 0 && (
+        <>
+          <h5>
+            If the data is not loaded, check the console to see the response
+            from the server for the unauthorized request.
+          </h5>
+          <p>Try get the data by changing the token in App.jsx.</p>
+          <p>Try to fetch data using Postman without using a frontend app.</p>
+        </>
+      )}
+      {users.length !== 0 && <MyList myList={users} />}
     </>
   );
 }
